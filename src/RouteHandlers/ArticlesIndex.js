82628647a4492852ae8c23c0bc5479aba08c7ff4
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-04-15 21:47:39 +0300

    add redirects and inex route

---
+++ src/RouteHandlers/ArticlesIndex.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react'
+
+class ArticlesIndex extends Component {
+    static propTypes = {
+
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>Select your article</h2>
+            </div>
+        )
+    }
+}
+
+export default ArticlesIndex
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,25 @@
 import React from 'react'
-import { Router, Route, hashHistory, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, IndexRedirect, hashHistory, browserHistory } from 'react-router'
 import IndexPage from './RouteHandlers/IndexPage'
 import Articles from './RouteHandlers/Articles'
 import ArticlePage from './RouteHandlers/ArticlePage'
 import NewArticlePage from './RouteHandlers/NewArticlePage'
 import CommentsRoot from './RouteHandlers/CommentsRoot'
 import CommentsPage from './RouteHandlers/CommentsPage'
+import ArticlesIndex from './RouteHandlers/ArticlesIndex'
 
 export default (
     <Router history = {browserHistory} >
+        <Redirect from = "/" to = "/articles" />
         <Route path="/" component = {IndexPage} >
             <Route path = "articles/new" component = {NewArticlePage} />
             <Route path = "articles" component = {Articles} >
+                <IndexRoute component = {ArticlesIndex} />
                 <Route path = "/new" component = {NewArticlePage} />
                 <Route path = ":id" component = {ArticlePage} />
             </Route>
             <Route path = "comments" component = {CommentsRoot} >
+                <IndexRedirect to = "1" />
                 <Route path = ":page" component = {CommentsPage} />
             </Route>
         </Route>
