ad2d364218039ebb1a5f57f7242752df3e4371b4
Alexei Zhuravski <alexei.zhuravski@gmail.com>
2016-04-29 17:21:24 +0200

    debugging tools are deleted

--- src/app.js
+++ src/app.js
@@ -4,7 +4,4 @@ import Root from './containers/Root'
 import store from './store'
 require('./sass/main.scss')
 
-// for debugging purposes only
-window.store = store
-
 render(<Root store = {store} />, document.getElementById('container'))
